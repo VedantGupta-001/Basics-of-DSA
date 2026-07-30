4a77a072452af749d2f5918a57f81c892a9aa9a6
VedantGupta-001 <guptavedant062008@gmail.com>
2026-07-30 10:11:50 +0530

    Array Program 2

---
+++ Array-Programs/array2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[3];
+    for (int i = 0; i < 3; i++)
+    {
+        cout << "Enter the " << i+1 << " element:";
+        cin >> arr[i];
+    }
+    cout << "\n";
+    int k[3];
+    for (int i = 0; i < 3; i++)
+    {
+        cout << "Enter the " << i+1 << " element:";
+        cin >> k[i];
+    }
+    cout << "\n";
+    int a[3];
+    for (int i = 0; i < 3; i++)
+    {
+        a[i] = arr[i] + k[i];
+        cout << a[i] << "\t";
+    }
+}
