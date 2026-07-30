3c52fe780f53c62ddaa70a1e115d2c2f6e939cf5
VedantGupta-001 <guptavedant062008@gmail.com>
2026-07-30 10:00:50 +0530

    Nested For Program 5

---
+++ Nested-For-Programs/nestedfor5.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{   
+    int n = 5;
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            cout << n;
+        }
+        cout << endl;
+        n--;
+    }
+}
