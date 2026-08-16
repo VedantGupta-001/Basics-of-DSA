78a69f0776a2754ce163c937accae8283348acba
VedantGupta-001 <guptavedant062008@gmail.com>
2026-08-16 22:19:57 +0530

    Doubly Linked List Insertion 3

---
+++ Linked-Lists/Doubly Linked List/insertionatend.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node * next;
+    Node * prev;
+    Node (int value){
+        data = value;
+        next = nullptr;
+        prev = nullptr;
+    }
+};
+
+void insertionatend(Node *& head, int value){
+    Node * newnode;
+    newnode = new Node(value);
+    newnode -> next = nullptr;
+    if (head == nullptr){
+        head = newnode;
+        newnode -> prev = nullptr;
+        return;
+    }
+    Node * temp = head;
+    while (temp -> next != nullptr){
+        temp = temp -> next;
+    }
+    temp -> next = newnode;
+    newnode -> prev = temp;
+}
+
+void display(Node * head){
+    Node * temp = head;
+    while (temp != nullptr){
+        cout << temp -> data << " ";
+        temp = temp -> next;
+    }
+    cout << endl;
+}
+
+int main(){
+    Node * head;
+    head =  new Node(10);
+    head -> next =  new Node(20);
+    head -> next -> next = new Node(30);
+    head -> next -> next -> next =  new Node(40);
+
+    int value;
+    cout << "Enter the value: ";
+    cin >> value;
+
+    insertionatend(head,value);
+
+    display(head);
+}
